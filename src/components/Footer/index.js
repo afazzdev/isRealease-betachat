ca6886e169fba1b3d62334ba5298d4b20ed1a1c6
Pondok Programmer <pondokit@Thinkpad-X200s>
2019-11-04 07:16:30 +0700

    fix bug nov 4 07.16

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,24 @@ const Footer = () => {
     <>
       <div className="Footer">
         {" "}
-        <span>&copy; Beta Team 2019</span>
+        <span>
+          &copy; Beta Team 2019 -{" "}
+          <a
+            target="_blank"
+            href="https://github.com/afazzdev/isRealease-betachat"
+            style={{ textDecoration: "none" }}
+          >
+            <i className="fab fa-github" style={{ color: "white" }}></i>{" "}
+            <span
+              style={{
+                textDecoration: "none",
+                color: "white"
+              }}
+            >
+              Github
+            </span>
+          </a>
+        </span>
       </div>
     </>
   );
